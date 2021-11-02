73fe73bc42374e6c4ecfca215258ced96a237067
sammwaura <meshsami27@gmail.com>
2021-11-02 12:21:47 +0300

    modify files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FiltersComponent } from './components/shopping-cart/filters/filters.com
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './components/shopping-cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shopping-cart/product-item/product-item.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { CartItemComponent } from './components/shopping-cart/cart-item/cart-ite
     FiltersComponent,
     ProductListComponent,
     CartComponent,
-    CartItemComponent
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
