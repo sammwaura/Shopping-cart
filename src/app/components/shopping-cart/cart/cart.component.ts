b7f824075ccaacaeedaa8e6fe5c40daed273f787
sammwaura <meshsami27@gmail.com>
2021-11-02 14:51:34 +0300

    modify files

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartItems = [
+    {id: 1, productId: 1, productName: 'Utd Kit', qty: 4, price: 1200 },
+    {id: 2, productId: 2, productName: 'Chels Kit', qty: 5, price: 1100 },
+    {id: 3, productId: 3, productName: 'Arsenal Kit', qty: 3, price: 1000 },
+    {id: 4, productId: 4, productName: 'Barca Kit', qty: 2, price: 1600 },
+  ];
+
+  cartTotal = 0
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cartItems.forEach(item =>{
+      this.cartTotal += (item.qty * item.price)
+    })
   }
 
 }
